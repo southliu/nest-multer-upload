6584527c60707aabdbf3d003ed1c769d956dc9b5
southliu <1275093225@qq.com>
2024-01-21 22:15:23 +0800

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,100 @@
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import {
+  AnyFilesInterceptor,
+  FileFieldsInterceptor,
+  FileInterceptor,
+  FilesInterceptor,
+} from '@nestjs/platform-express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('aaa')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile3(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 5000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+
+  @Post('bbb')
+  @UseInterceptors(
+    FilesInterceptor('bbb', 3, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  @Post('ccc')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'aaa', maxCount: 2 },
+        { name: 'bbb', maxCount: 3 },
+      ],
+      {
+        dest: 'uploads',
+      },
+    ),
+  )
+  uploadFileFields(
+    @UploadedFiles()
+    files: { aaa?: Express.Multer.File[]; bbb?: Express.Multer.File[] },
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  @Post('ddd')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads',
+    }),
+  )
+  uploadAnyFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+}
